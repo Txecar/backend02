73016f715950a45bd66b1c502c25650a86fb2c45
Jose Carlos <jose-carlos.gonzalez-vidal@capgemini.com>
2022-01-17 14:02:49 +0100

    Primer commit

---
+++ src/main/java/com/capgemini/ProyectoBackendtest02Application.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBackendtest02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBackendtest02Application.class, args);
+	}
+
+}
